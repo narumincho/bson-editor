365fd6525d898df5f0d5514de1ec207ffa4315ce
narumincho <16481886+narumincho@users.noreply.github.com>
2023-07-25 20:22:13 +0900

    add build script

---
+++ build.ts
@@ -0,0 +1,93 @@
+import { fromFileUrl } from "https://deno.land/std@0.195.0/path/posix.ts";
+import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.1/mod.ts";
+import { build as esBuild } from "https://deno.land/x/esbuild@v0.18.11/mod.js";
+import { ensureFile } from "https://deno.land/std@0.195.0/fs/mod.ts";
+
+/**
+ * ```bash
+ * deno run -A ./build.ts
+ * ```
+ */
+
+/** */
+export const writeTextFileWithLog = async (
+  path: URL,
+  content: string
+): Promise<void> => {
+  console.log(path.toString() + " に書き込み中... " + content.length + "文字");
+  await ensureFile(path);
+  await Deno.writeTextFile(path, content);
+  console.log(path.toString() + " に書き込み完了!");
+};
+
+const distributionPath = new URL(
+  "./vscodeExtensionDistribution/",
+  import.meta.url
+);
+
+const build = async (): Promise<string> => {
+  const esbuildResult = await esBuild({
+    entryPoints: [fromFileUrl(new URL("./main.ts", import.meta.url))],
+    plugins: denoPlugins(),
+    write: false,
+    bundle: true,
+    format: "cjs",
+    target: ["node18"],
+  });
+
+  for (const esbuildResultFile of esbuildResult.outputFiles ?? []) {
+    if (esbuildResultFile.path === "<stdout>") {
+      console.log("js 発見");
+      const scriptContent = new TextDecoder().decode(
+        esbuildResultFile.contents
+      );
+
+      return scriptContent;
+    }
+  }
+  throw new Error("esbuild で <stdout> の出力を取得できなかった...");
+};
+
+const scriptRelativePath = "./main.js";
+
+writeTextFileWithLog(
+  new URL(scriptRelativePath, distributionPath),
+  await build()
+);
+
+writeTextFileWithLog(
+  new URL("./package.json", distributionPath),
+  JSON.stringify({
+    name: "bson-editor",
+    version: "0.0.1",
+    description: "bson-editor VSCode extension",
+    repository: {
+      url: "git+https://github.com/narumincho/bson-editor.git",
+      type: "git",
+    },
+    license: "MIT",
+    homepage: "https://github.com/narumincho/bson-editor",
+    author: "narumincho",
+    engines: {
+      vscode: "^1.76.0",
+    },
+    dependencies: {},
+    activationEvents: [],
+    contributes: {
+      commands: [
+        {
+          command: "extension.helloWorld",
+          title: "Hello World",
+        },
+      ],
+    },
+    browser: scriptRelativePath,
+    publisher: "narumincho",
+  })
+);
+
+writeTextFileWithLog(
+  new URL("README.md", distributionPath),
+  `bson-editor VSCode extension
+`
+);
---
+++ main.ts
@@ -0,0 +1,26 @@
+import {
+  ExtensionContext,
+  importVsCodeApi,
+} from "https://deno.land/x/vscode@1.80.0/mod.ts";
+
+export function activate(context: ExtensionContext) {
+  const vscode = importVsCodeApi();
+  if (vscode === undefined) {
+    throw new Error(
+      "Could not import vscode api because it was not working within the extension"
+    );
+  }
+
+  console.log(
+    'Congratulations, your extension "helloworld-sample" is now active!'
+  );
+
+  const disposable = vscode.commands.registerCommand(
+    "extension.helloWorld",
+    () => {
+      vscode.window.showInformationMessage("Hello World!");
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
