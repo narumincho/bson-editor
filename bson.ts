126e189d57855b9fdd5beb6d4cf602cb3529987b
narumincho <16481886+narumincho@users.noreply.github.com>
2023-08-05 17:13:01 +0900

    一部エラーでも解釈できるように

--- bson.ts
+++ bson.ts
@@ -1,176 +1,228 @@
-export type DeserializeResult<T> = {
-  readonly type: "ok";
-  readonly value: T;
-} | {
-  readonly type: "error";
-  readonly error:
-    | SizeTooSmallError
-    | BinarySizeError
-    | IllegalEndValue
-    | UnsupportedType;
-};
-
 export type SizeTooSmallError = {
   readonly type: "sizeTooSmall";
   readonly size: number;
+  readonly min: number;
 };
 
-export type BinarySizeError = {
-  readonly type: "binarySizeError";
-  readonly expectedSize: number;
-  readonly actualSize: number;
+export type ErrorAtSize = {
+  readonly type: "cannotReadSize";
+} | {
+  readonly type: "lessThanOrEqualZero";
 };
 
-export type IllegalEndValue = {
-  readonly type: "illegalEndValue";
+export type NotFoundEndOfFlagError = {
   readonly index: number;
-  readonly value: number;
+  readonly value: number | undefined;
 };
 
-export type UnsupportedType = {
+export type ElementValueWithInvalid =
+  | { readonly type: "double"; readonly value: DoubleWithInvalid }
+  | { readonly type: "string"; readonly value: StringWithInvalid }
+  | { readonly type: "document"; readonly value: DocumentWithInvalid }
+  | { readonly type: "int32"; readonly value: Int32WithInvalid };
+
+export type DoubleWithInvalid = {
+  readonly type: "ok";
+  readonly value: number;
+  readonly startIndex: number;
+  readonly endIndex: number;
+} | {
+  readonly type: "binarySizeTooSmall";
+};
+
+export type CStringWithInvalid = {
+  readonly value: string;
+  readonly startIndex: number;
+  readonly endIndex: number;
+  readonly originalIfInvalidUtf8Error: Uint8Array | undefined;
+  readonly notFoundEndOfFlag: boolean;
+};
+
+export type StringWithInvalid = {
+  readonly value: string;
+  readonly startIndex: number;
+  readonly endIndex: number;
+  readonly originalIfError: Uint8Array | undefined;
+  readonly warnNotFoundEndOfFlag: NotFoundEndOfFlagError | undefined;
+} | {
+  readonly type: "errorAtSize";
+  readonly error: ErrorAtSize;
+};
+
+export type DocumentWithInvalid = {
+  readonly type: "ok";
+  readonly value: ReadonlyArray<ElementWithInvalid>;
+  readonly warnNotFoundEndOfFlag: NotFoundEndOfFlagError | undefined;
+} | {
+  readonly type: "errorAtSize";
+  readonly error: ErrorAtSize;
+};
+
+export type ElementWithInvalid = {
+  readonly type: "ok";
+  readonly value: {
+    readonly name: CStringWithInvalid;
+    readonly value: ElementValueWithInvalid;
+  };
+} | {
   readonly type: "unsupportedType";
   readonly typeId: number;
 };
 
-export type StructuredBson = BsonDouble | StructuredBsonArray;
-
-export type BsonDouble = {
-  readonly type: "double";
+export type Int32WithInvalid = {
+  readonly type: "ok";
   readonly value: number;
-};
-
-export type StructuredBsonArray = {
-  readonly type: "array";
-  readonly value: ReadonlyArray<
-    { readonly name: string; readonly value: StructuredBson }
-  >;
+  readonly startIndex: number;
+  readonly endIndex: number;
+} | {
+  readonly type: "binarySizeTooSmall";
 };
 
 export const bsonBinaryToStructuredBson = (
   binary: Uint8Array,
-): DeserializeResult<StructuredBsonArray> => {
-  if (binary.length < 4) {
-    return {
-      type: "error",
-      error: { type: "sizeTooSmall", size: binary.length },
-    };
-  }
-  const size = parseInt32(binary, 0);
-  const sizeValue = size.value;
-  if (sizeValue < 5) {
-    return { type: "error", error: { type: "sizeTooSmall", size: size.value } };
-  }
-
-  if (sizeValue > binary.length) {
-    return {
-      type: "error",
-      error: {
-        type: "binarySizeError",
-        expectedSize: sizeValue,
-        actualSize: binary.length,
-      },
-    };
-  }
-
-  if (binary[sizeValue - 1] !== 0) {
-    return {
-      type: "error",
-      error: {
-        type: "illegalEndValue",
-        index: sizeValue - 1,
-        value: binary[sizeValue - 1],
-      },
-    };
-  }
-  return deserializeObject(binary, size.nextIndex);
+): DocumentWithInvalid => {
+  return deserializeDocument(binary, 0);
 };
 
-const deserializeObject = (
+const deserializeDocument = (
   binary: Uint8Array,
-  index: number,
-): DeserializeResult<StructuredBsonArray> => {
-  const fields: Array<{
-    readonly name: string;
-    readonly value: StructuredBson;
-  }> = [];
+  index: 0,
+): DocumentWithInvalid => {
+  const bytesSize = parseInt32(binary, index);
+
+  if (bytesSize.type === "binarySizeTooSmall") {
+    return {
+      type: "errorAtSize",
+      error: {
+        type: "cannotReadSize",
+      },
+    };
+  }
+  const endOfFlagIndex = index + bytesSize.value;
+  const warnNotFoundEndOfFlag: NotFoundEndOfFlagError | undefined =
+    binary[endOfFlagIndex] === 0 ? undefined : {
+      index: endOfFlagIndex,
+      value: binary[endOfFlagIndex],
+    };
+
+  const elements: Array<ElementWithInvalid> = [];
+  let nextIndex = bytesSize.endIndex + 1;
   while (true) {
-    const field = deserializeObjectField(binary, index);
-    if (field.type === "error") {
-      return field;
-    }
-    if (field.value === undefined) {
+    const element = deserializeElement(binary, nextIndex);
+
+    if (element.type === "endOfFlag" || element.type === "empty") {
       return {
         type: "ok",
-        value: { type: "array", value: fields },
+        value: elements,
+        warnNotFoundEndOfFlag,
       };
     }
-    fields.push({ name: field.value.name, value: field.value.value });
+    elements.push(element.value);
+    nextIndex = element.nextIndex;
   }
 };
 
-const typeDouble = 0x01;
+const typeIdDouble = 0x01;
+const typeIdString = 0x02;
 
-const deserializeObjectField = (
+const deserializeElement = (
   binary: Uint8Array,
   index: number,
-): DeserializeResult<
-  {
-    name: string;
-    value: StructuredBson;
-    nextIndex: number;
-  } | undefined
-> => {
-  const type = binary[index];
-  const valueIndex = index + 1;
-  const nameAndNextIndex = parseCString(binary, valueIndex);
-  switch (type) {
-    case 0: {
-      return { type: "ok", value: undefined };
-    }
-    case typeDouble: {
-      const dataView = new DataView(binary.buffer);
+): {
+  readonly type: "ok";
+  readonly value: ElementWithInvalid;
+  readonly nextIndex: number;
+} | {
+  readonly type: "endOfFlag";
+  readonly nextIndex: number;
+} | {
+  readonly type: "empty";
+} => {
+  const typeId = binary[index];
+  if (typeId === undefined) {
+    return { type: "empty" };
+  }
+  if (typeId === 0) {
+    return { type: "endOfFlag", nextIndex: index + 1 };
+  }
+  const nameAndNextIndex = parseCString(binary, index + 1);
+  const dataView = new DataView(binary.buffer, nameAndNextIndex.endIndex + 1);
+  switch (typeId) {
+    case typeIdDouble: {
       return {
         type: "ok",
         value: {
-          name: nameAndNextIndex.value,
+          name: nameAndNextIndex,
           value: {
             type: "double",
-            value: dataView.getFloat64(nameAndNextIndex.nextIndex),
+            value: dataView.getFloat64(0),
           },
           nextIndex: nameAndNextIndex.nextIndex + 8,
         },
       };
     }
+    case typeIdString: {
+    }
   }
   return {
-    type: "error",
-    error: { type: "unsupportedType", typeId: type },
+    type: "ok",
+    value: {
+      type: "unsupportedType",
+      typeId,
+    },
   };
 };
 
 const parseInt32 = (
   binary: Uint8Array,
   index: number,
-): { readonly value: number; readonly nextIndex: number } => {
-  return {
-    value: binary[index] |
-      (binary[index + 1] << 8) |
-      (binary[index + 2] << 16) |
-      (binary[index + 3] << 24),
-    nextIndex: index + 4,
-  };
+): Int32WithInvalid => {
+  try {
+    return {
+      type: "ok",
+      value: new DataView(binary.buffer).getInt32(index, true),
+      startIndex: index,
+      endIndex: index + 4,
+    };
+  } catch (_e) {
+    return {
+      type: "binarySizeTooSmall",
+    };
+  }
 };
 
 const parseCString = (
   binary: Uint8Array,
   index: number,
-): { readonly value: string; readonly nextIndex: number } => {
+): CStringWithInvalid => {
   const endIndex = binary.indexOf(0, index);
-  if (endIndex === -1) {
-    throw new Error("Could not find end of cstring");
+  const notFoundEndOfFlag = endIndex === -1;
+  const sliced = binary.slice(index, notFoundEndOfFlag ? undefined : endIndex);
+  try {
+    return {
+      value: new TextDecoder(undefined, { fatal: true }).decode(
+        sliced,
+      ),
+      originalIfInvalidUtf8Error: undefined,
+      startIndex: index,
+      endIndex: notFoundEndOfFlag ? binary.length - 1 : endIndex,
+      notFoundEndOfFlag,
+    };
+  } catch (_e) {
+    return {
+      value: new TextDecoder().decode(
+        sliced,
+      ),
+      originalIfInvalidUtf8Error: sliced,
+      startIndex: index,
+      endIndex: notFoundEndOfFlag ? binary.length - 1 : endIndex,
+      notFoundEndOfFlag,
+    };
   }
-  return {
-    value: new TextDecoder().decode(binary.slice(index, endIndex)),
-    nextIndex: endIndex + 1,
-  };
+};
+
+const parseString = (dataView: DataView): StringWithInvalid => {
+};
+
+const parseDouble = (dataView: DataView): DoubleWithInvalid => {
 };
